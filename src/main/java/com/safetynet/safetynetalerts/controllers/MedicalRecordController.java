21915f6e97cc8c4530d77091bdfafb0f467cfacb
yaya <cherifi.yahia@yahoo.fr>
2020-06-21 16:17:30 +0200

    create, update and delete operations added. MedicalRecordController, MedicalRecordService and MedicalRecordRepository created

---
+++ src/main/java/com/safetynet/safetynetalerts/controllers/MedicalRecordController.java
@@ -0,0 +1,53 @@
+package com.safetynet.safetynetalerts.controllers;
+
+import com.safetynet.safetynetalerts.domain.MedicalRecord;
+import com.safetynet.safetynetalerts.service.MedicalRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicalRecord")
+public class MedicalRecordController {
+
+    //inject medicalRecordService
+    private MedicalRecordService medicalRecordService;
+
+    @Autowired
+    public MedicalRecordController(MedicalRecordService medicalRecordService) {
+        this.medicalRecordService = medicalRecordService;
+    }
+
+    //add mapping for POST /medicalRecord/add
+    @PostMapping("/add")
+    public MedicalRecord addMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
+        //in case of passing an id in the json.. we have to set it to 0
+        //mainly used to force saving a new item instead of updating
+        //an existing item that has the id entered in the json
+        medicalRecord.setId(0L);
+        medicalRecordService.save(medicalRecord);
+        return medicalRecord;
+    }
+
+    //add mapping for PUT /medicalRecord/update
+    @PutMapping("/update")
+    public MedicalRecord updateMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
+        medicalRecordService.save(medicalRecord);
+        return medicalRecord;
+    }
+
+    //add mapping for DELETE /medicalRecord/delete
+    @DeleteMapping("/delete/{firstName}&{lastName}")
+    public String deleteMedicalRecord(@PathVariable String firstName, @PathVariable String lastName) {
+
+        MedicalRecord medicalRecord = medicalRecordService.findByFirstNameAndLastName(firstName, lastName);
+
+        if (medicalRecord == null) {
+            throw new RuntimeException("No medical record that belongs to '" +
+                    firstName + " " + lastName + "' was found");
+        }
+
+        medicalRecordService.deleteByFirstNameAndLastName(firstName, lastName);
+
+        return ("Medical records for '" + firstName + " " + lastName + "' were deleted successfully");
+    }
+}
---
+++ src/main/java/com/safetynet/safetynetalerts/service/MedicalRecordServiceImpl.java
@@ -0,0 +1,33 @@
+package com.safetynet.safetynetalerts.service;
+
+import com.safetynet.safetynetalerts.domain.MedicalRecord;
+import com.safetynet.safetynetalerts.repository.MedicalRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MedicalRecordServiceImpl implements MedicalRecordService {
+
+    //inject MedicalRecordRepository
+    private MedicalRecordRepository medicalRecordRepository;
+
+    @Autowired
+    public MedicalRecordServiceImpl(MedicalRecordRepository medicalRecordRepository) {
+        this.medicalRecordRepository = medicalRecordRepository;
+    }
+
+    @Override
+    public void save(MedicalRecord medicalRecord) {
+        medicalRecordRepository.save(medicalRecord);
+    }
+
+    @Override
+    public void deleteByFirstNameAndLastName(String firstName, String lastName) {
+        medicalRecordRepository.deleteByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @Override
+    public MedicalRecord findByFirstNameAndLastName(String firstName, String lastName) {
+        return medicalRecordRepository.findByFirstNameAndLastName(firstName, lastName);
+    }
+}
