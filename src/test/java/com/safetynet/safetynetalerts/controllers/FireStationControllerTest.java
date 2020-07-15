25395d02fcca474c718904f6595392a77ffcc6e3
yaya <cherifi.yahia@yahoo.fr>
2020-07-15 16:26:29 +0200

    tests for FireStationController added.

---
+++ src/test/java/com/safetynet/safetynetalerts/controllers/FireStationControllerTest.java
@@ -0,0 +1,126 @@
+package com.safetynet.safetynetalerts.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.safetynetalerts.domain.FireStation;
+import com.safetynet.safetynetalerts.service.FireStationService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(FireStationController.class)
+class FireStationControllerTest {
+
+    FireStationController fireStationController;
+
+    @MockBean
+    FireStationService fireStationService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        fireStationController = new FireStationController(fireStationService);
+    }
+
+    @AfterEach
+    void tearDown() {
+        fireStationController = null;
+    }
+
+    @Test
+    void findFireStations() throws Exception {
+        List<FireStation> fireStations = new ArrayList<>();
+        FireStation fireStation = new FireStation();
+
+        fireStation.setId(1L);
+        fireStation.setAddress("an address");
+        fireStation.setStation(3);
+        fireStations.add(fireStation);
+
+        when(fireStationService.findAll()).thenReturn(fireStations);
+
+        mockMvc.perform(get("/fireStation/fireStations")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].address").value("an address"))
+                .andExpect(jsonPath("$[0].station").value(3));
+    }
+
+    @Test
+    void addFireStation() throws Exception {
+        FireStation fireStation = new FireStation();
+        fireStation.setId(1L);
+        fireStation.setAddress("an address");
+        fireStation.setStation(3);
+        when(fireStationService.save(any(FireStation.class))).thenReturn(fireStation);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/fireStation/add")
+                .content(new ObjectMapper().writeValueAsString(fireStation))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.address").value("an address"))
+                .andExpect(jsonPath("$.station").value(3));
+    }
+
+    @Test
+    void updateFireStation() throws Exception {
+        FireStation fireStation = new FireStation();
+        fireStation.setId(1L);
+        fireStation.setAddress("an address");
+        fireStation.setStation(3);
+        when(fireStationService.save(any(FireStation.class))).thenReturn(fireStation);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/fireStation/update")
+                .content(new ObjectMapper().writeValueAsString(fireStation))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.address").value("an address"))
+                .andExpect(jsonPath("$.station").value(3));
+    }
+
+    @Test
+    void deleteFireStation() throws Exception{
+        FireStation fireStation = new FireStation();
+        fireStation.setAddress("an address");
+        fireStation.setStation(3);
+
+        when(fireStationService.findById(anyLong())).thenReturn(Optional.of(fireStation));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/fireStation/delete/1"))
+                .andExpect(status().isOk());
+    }
+}
