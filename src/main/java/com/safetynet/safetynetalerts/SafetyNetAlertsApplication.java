1db4d357f4edccddacf0915afbd40e2283036163
yaya <cherifi.yahia@yahoo.fr>
2020-08-09 21:05:59 +0200

    SafetyNetAlertsApplication.java checked and corrected.

--- src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
@@ -7,26 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SafetyNetAlertsApplication {
+    /**
+     * SafetyNetAlertsApplication logger.
+     */
+    private static final Logger LOGGER = LogManager
+            .getLogger(SafetyNetAlertsApplication.class);
 
-	/**
-	 * SafetyNetAlertsApplication logger.
-	 */
-	private static final Logger LOGGER = LogManager
-			.getLogger(SafetyNetAlertsApplication.class);
+    /**
+     * A protected class constructor.
+     */
+    protected SafetyNetAlertsApplication() {
+    }
 
-	/**
-	 * A protected class constructor.
-	 */
-	protected SafetyNetAlertsApplication() {
-	}
-
-	/**
-	 * The main method's role is launching the app.
-	 * @param args args
-	 */
-	public static void main(final String[] args) {
-		LOGGER.debug("SafetyNet alert started");
-		SpringApplication.run(SafetyNetAlertsApplication.class, args);
-	}
+    /**
+     * The main method's role is launching the app.
+     * @param args args
+     */
+    public static void main(final String[] args) {
+        LOGGER.debug("SafetyNet alert started");
+        SpringApplication.run(SafetyNetAlertsApplication.class, args);
+    }
 
 }
